23f6367479b39ca1d7e8f62ef4f22fca45696136
emmaglorypraise <emmaglorypraise@gmail.com>
2020-08-19 01:03:54 +0100

    added some javascript code

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var bust = document.getElementById('bust').value;
+var waist = document.getElementById('waist').value;
+var hips = document.getElementById('hips').value;
+function whenClicked() {
+  var bust = document.getElementById('bust').value;
+  var waist = document.getElementById('waist').value;
+  var hips = document.getElementById('hips').value;
+
+  console.log(bust, waist, hips);
+}
